47e76d9fa85f6fbbc75a8de4a4e97f96fca14d9a
unknown <hzqibinchuan@hz.ntes.domain>
2017-02-07 16:40:07 +0800

    Signed-off-by: unknown <hzqibinchuan@hz.ntes.domain>

---
+++ src/caffe/layers/accuracy_layer.cpp
@@ -0,0 +1,79 @@
+#include <functional>
+#include <utility>
+#include <vector>
+
+#include "caffe/layers/accuracy_layer.hpp"
+#include "caffe/util/math_functions.hpp"
+
+namespace caffe {
+
+template <typename Dtype>
+void AccuracyLayer<Dtype>::LayerSetUp(
+  const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+  top_k_ = this->layer_param_.accuracy_param().top_k();
+
+  has_ignore_label_ =
+    this->layer_param_.accuracy_param().has_ignore_label();
+  if (has_ignore_label_) {
+    ignore_label_ = this->layer_param_.accuracy_param().ignore_label();
+  }
+}
+
+
+template <typename Dtype>
+void AccuracyLayer<Dtype>::Reshape(
+  const vector<Blob<Dtype>*>& bottom, const vector<Blob<Dtype>*>& top) {
+
+  CHECK_EQ(bottom[0]->num(), bottom[1]->num())
+      << "The data and label should have the same number.";
+  CHECK_LE(top_k_, bottom[0]->count() / bottom[0]->num())
+      << "top_k must be less than or equal to the number of classes.";
+  //CHECK_EQ(bottom[1]->channels(), 1);
+  CHECK_EQ(bottom[1]->height(), 1);
+  CHECK_EQ(bottom[1]->width(), 1);
+  top[0]->Reshape(1, 1, 1, 1);
+}
+
+template <typename Dtype>
+void AccuracyLayer<Dtype>::Forward_cpu(const vector<Blob<Dtype>*>& bottom,
+    const vector<Blob<Dtype>*>& top) {
+
+  Dtype accuracy = 0;
+  const Dtype* bottom_data = bottom[0]->cpu_data();
+  const Dtype* bottom_label = bottom[1]->cpu_data();
+  int num = bottom[0]->num();
+  int dim = bottom[0]->count() / bottom[0]->num();
+  vector<Dtype> maxval(top_k_+1);
+  vector<int> max_id(top_k_+1);
+  for (int i = 0; i < num; ++i) {
+    // Top-k accuracy
+    std::vector<std::pair<Dtype, int> > bottom_data_vector;
+    std::vector<Dtype> bottom_label_vector;
+    for (int j = 0; j < dim; ++j) {
+      bottom_data_vector.push_back(
+          std::make_pair(bottom_data[i * dim + j], j));
+      bottom_label_vector.push_back(bottom_label[i * dim + j]);
+    }
+    std::partial_sort(
+        bottom_data_vector.begin(), bottom_data_vector.begin() + top_k_,
+        bottom_data_vector.end(), std::greater<std::pair<Dtype, int> >());
+    // check if true label is in top k predictions
+    for (int k = 0; k < top_k_; k++) {
+      if (bottom_data_vector[k].second == std::distance(bottom_label_vector.begin(), std::max_element(bottom_label_vector.begin(), bottom_label_vector.end()))) {
+        ++accuracy;
+        break;
+      }
+    }
+  }
+
+  // LOG(INFO) << "Accuracy: " << accuracy;
+  top[0]->mutable_cpu_data()[0] = accuracy / num;
+  // Accuracy layer should not be used as a loss function.
+}
+
+
+
+INSTANTIATE_CLASS(AccuracyLayer);
+REGISTER_LAYER_CLASS(Accuracy);
+
+}  // namespace caffe
